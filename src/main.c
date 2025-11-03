72f76447997be1a56ceaac6abbaf5c93e98e6fd1
Francesco Garbin <francescogarbin@gmail.com>
2025-11-03 20:57:41 +0100

    Hush-hush! First commit, playing with UTF-xx in C23

--- src/main.c
+++ src/main.c
@@ -1,14 +1,12 @@
 #include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 #include <uchar.h>
+#include <wchar.h>
 
 int main(int argc, char* argv[]) {
     setlocale(LC_ALL, "");
-    wchar_t * salutation = NULL;
-    salutation = malloc(sizeof(char16_t)*strlen("Hello, pvenom-tui!\n")+1);
-    printf("Hello, pvenom-tui!");
-    free(salutation);
+    wchar_t str[] = L"Hello, pvenom-tui! \U0001F92B\n";
+    printf("%ls", str);  // %ls converte wchar_t* → char* UTF-8
     return EXIT_SUCCESS;
 }
