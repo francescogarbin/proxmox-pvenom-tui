c1187949b393396140e93f8c405e483fb8aa1c69
Francesco Garbin <francescogarbin@gmail.com>
2025-11-04 07:41:37 +0100

    Small changes

--- src/main.c
+++ src/main.c
@@ -1,12 +1,10 @@
 #include <locale.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <uchar.h>
-#include <wchar.h>
 
 int main(int argc, char* argv[]) {
     setlocale(LC_ALL, "");
-    wchar_t str[] = L"Hello, pvenom-tui! \U0001F92B\n";
-    printf("%ls", str);  // %ls converte wchar_t* → char* UTF-8
+    wchar_t* str = L"Hello, pvenom-tui! \U0001F92B\n";
+    printf("%ls", str);
     return EXIT_SUCCESS;
 }
