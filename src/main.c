5647d474488c36bb464909080337891f0a7c115a
Francesco Garbin <francescogarbin@gmail.com>
2025-11-04 20:03:40 +0100

    Warming up with wchar_t for UTF-16 encodings.

--- src/main.c
+++ src/main.c
@@ -2,9 +2,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#include <string.h>
+#include <uchar.h>
+#include <wchar.h>
+
 int main(int argc, char* argv[]) {
     setlocale(LC_ALL, "");
-    wchar_t* str = L"Hello, pvenom-tui! \U0001F92B\n";
-    printf("%ls", str);
+    wchar_t str[300];
+    wcscpy(str, L"Hello, pvenom-tui! \U0001F92B What is your name?:");
+    wprintf(L"%ls ", str);
+    wchar_t* input = malloc(sizeof(wchar_t)*256);
+    fgetws(input, 256, stdin);
+    const unsigned long length = wcslen(input);
+    wprintf(L"%ls - length: %lu\n", input, length);
+    free(input);
     return EXIT_SUCCESS;
 }
